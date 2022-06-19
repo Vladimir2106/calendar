a2793e520309fb1f8303f641547d0b741b6d9b25
Vladimir2106 <107151383+Vladimir2106@users.noreply.github.com>
2022-06-19 15:08:22 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"></link>
+    <title>CALENDAR</title>
+</head>
+<body>
+<div class="container">
+    <!header>
+    <div class="central_panel">
+        <div class="header">
+            <span class="title">22 March 2018</span>
+            <div class="search_icon">
+                <div class="Oval"></div>
+                <div class="Shape"></div>  
+            </div>
+           <!div class="layout_icon">
+                <div id="Rectangle-path1"></div>
+                <div id="Rectangle-path2"></div>
+                <div id="Rectangle-path3"></div>
+                <div id="Rectangle-path4"></div>
+           <!/div>
+            <div class="line1"></div>
+        </div>
+
+        <!red_bloc>
+        <div class="red_bloc">
+            <div class="text1">
+                <span #="text11">Meet Sophia in airport</span>
+            </div>
+            <div class="side1">
+                <div class="Rectangle2"></div>
+            </div>
+            <div class="background1"></div>
+        </div>
+
+        <!blue_bloc>
+        <div class="blue_bloc">
+            <div class="text2">
+                <span #="text22">Studio workshop</span>
+            </div>
+            <div class="side2">
+                <div class="Rectangle3"></div>
+            </div>
+            <div class="background2"></div>
+        </div>
+
+        <!orange_bloc>
+        <div class="orange_bloc">
+            <div class="text3">
+                <span #="text33">Studio workshop</span>
+            </div>
+            <div class="side3">
+                <div class="Rectangle4"></div>
+            </div>
+            <div class="background4"></div>
+        </div>
+
+        <!lines>   
+        <div class="lines">    
+            <div class="hov01">
+                <div class="hov1">20:00</div>
+                <div class="hov2"></div>
+            </div>
+            <div class="hov20">
+                <div class="hov1">19:00</div>
+                <div class="hov2"></div>
+            </div>
+            <div class="hov30">
+                <div class="hov1">18:00</div>
+                <div class="hov2"></div>
+            </div>
+            <div class="hov40">
+                <div class="hov1">17:00</div>
+                <div class="hov2"></div>
+            </div>
+            <div class="hov50">
+                <div class="hov1">16:00</div>
+                <div class="hov2"></div>
+            </div>
+            <div class="hov60">
+                <div class="hov1">15:00</div>
+                <div class="hov2"></div>
+            </div>
+            <div class="hov70">
+                <div class="hov1">14:00</div>
+                <div class="hov2"></div>
+            </div>
+            <div class="hov80">
+                <div class="hov1">13:00</div>
+                <div class="hov2"></div>
+            </div>
+            <div class="hov90">
+                <div class="hov1">12:00</div>
+                <div class="hov2"></div>
+            </div>
+            <div class="hov10">
+                <div class="hov1">11:00</div>
+                <div class="hov2"></div>
+            </div>
+            <div class="hov11">
+                <div class="hov1">10:00</div>
+                <div class="hov2"></div>
+            </div>
+            <div class="hov12">
+                <div class="hov1">9:00</div>
+                <div class="hov22"></div>
+            </div>
+
+        </div>
+
+        <div class="Rectangle"></div>
+    </div>
+
+    <div class="right_panel ">
+        <div class="separation">
+            <div id="black"></div>
+            <div id="white"></div>
+        </div>
+
+        <div class="calendar">
+            <div class="month"><p>March 2018</p>
+                <div id="prev_icon"></div>
+                <div id="next-icon"></div>
+            </div>
+
+            <div class="days">
+                <div id="Sum"></div>
+                <div id="Sat"></div>
+                <div id="F"></div>
+                <div id="T"></div>
+                <div id="W"></div>
+                <div id=T2></div>
+                <div id="M"></div>
+                <div id="BG"></div>
+                <div id="black2"></div>
+                <div id="white2"></div>
+            </div>
+
+            <div class="line3">
+                <div id="l31">1</div>
+                <div id="l32">2</div>
+                <div id="l33">3</div>
+                <div id="l34">4</div>
+                <div id="l35">5</div>
+                <div id="l36">6</div>
+                <div id="l37">7</div>
+            </div>
+
+            <div class="line4">
+                <div id="l31">8</div>
+                <div id="l32">9</div>
+                <div id="l33">10</div>
+                <div id="l34">11</div>
+                <div id="l35">12</div>
+                <div id="l36">13</div>
+                <div id="l37">14</div>
+            </div>
+
+            <div class="line5">
+                <div id="l31">15</div>
+                <div id="oval11"></div>
+                <div id="oval12"></div>
+                <div id="l32">16</div>
+                <div id="l33">17</div>
+                <div id="l34">18</div>
+                <div id="l35">19</div>
+                <div id="l36">20</div>
+                <div id="l37">21</div>
+            </div>
+
+            <div class="line6">
+                <div id="l31">22</div>
+                <div id="l32">23</div>
+                <div id="l33">24</div>
+                <div id="l34">25</div>
+                <div id="l35">26</div>
+                <div id="l36">27</div>
+                <div id="l37">28</div>
+            </div>
+
+            <div class="line7">
+                <div id="l31">29</div>
+                <div id="l32">30</div>
+                <div id="l33">31</div>
+                <div id="l34">1</div>
+                <div id="l35">2</div>
+                <div id="l36">3</div>
+                <div id="l37">4</div>
+            </div>
+            
+        </div>   
+        <div class="categories"> <p>Your categories</p>
+                <!div class="separation1">
+                    <div id="black4"></div>
+                    <div id="white4"></div>
+                <!/div>
+                <div class="FINANCE"> <p> FINANCE</p></div>
+                <div  class="management"><p>MANAGEMENT</p></div>
+                <div class="design"><p>DESIGN</p></div>
+        </div>
+
+        <div class="color">
+                    <div id="Noise"></div>
+                    <div id="Overlay"></div>
+                    <div id="Exclusion"></div>
+                    <div id="blur"></div>
+        </div>
+
+    </div>
+
+
+
+</div>
+
+</body>
+</html>
